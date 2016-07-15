081285c560cb8510a45fc68f894cbc19f00140bd
Anh Truong <anh.truong.07@cnu.edu>
2016-07-15 09:42:45 -0400

    changes by tupac

---
+++ js/compliments/compliments.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+  var app = angular.module("compliments");
+  app.controller("complimentsController", complimentsController);
+
+  function ComplimentsController(){
+    var vm = this;
+  }
+})();
